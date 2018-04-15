5c49f93d9def5bf74dc572aeccb73a57d72477d2
gsoules <gsoules@avantlogic.com>
2018-04-15 19:41:21 -0400

    Add public_footer hook.

--- common/footer.php
+++ common/footer.php
@@ -4,6 +4,8 @@
             <div id="footer-text">
 				<?php echo get_theme_option('Footer Text'); ?>
 			</div>
+
+           <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
         </footer>
 
     </div><!-- end wrap -->
