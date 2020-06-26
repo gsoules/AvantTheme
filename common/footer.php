20a70490a75a2965bea15ecc557db7e163df87e9
gsoules <gsoules@avantlogic.com>
2020-06-26 18:50:34 -0400

    Emit footer links without using content from theme footer option.

--- common/footer.php
+++ common/footer.php
@@ -3,39 +3,30 @@
        <footer id="footer" role="contentinfo">
             <div id="footer-text">
                 <?php
+                $avantLogicLink = "Created by <a href='http://avantlogic.com/' target='_blank' title='AvantLogic Corporation' rel='noreferrer noopener'>AvantLogic</a>";
+                $loginUrl = url('users/login');
+                $logoutUrl = url('users/logout');
+
+                // Emit both login and logout links, but only one will appear depending whether the 'logged-out'
+                // or the 'logged-in' class has been dynamically emitted by AvantAdmin::emitDynamicCss().
+                $loginLogoutLink = "<a class='logged-out' href='$loginUrl'>Login</a><a class='logged-in' href='$logoutUrl'>Logout</a>";
+
+                $copyright = "© " . date("Y");
+                $siteTitle = get_option('site_title');
+                $footer = "$copyright $siteTitle — $avantLogicLink — $loginLogoutLink";
+
                 $themeOptionFooterText = get_theme_option('Footer Text');
-                if (plugin_is_active('AvantElasticsearch'))
+                if ($themeOptionFooterText)
                 {
-                    $parts = explode('|', $themeOptionFooterText);
-                    if (count($parts) != 2)
-                    {
-                        // The Footer option on the AvantTheme configuration page should contain a single line
-                        // of the form "<organization-name>|<organization-url>". If the organization has no URL,
-                        // the second value can be left blank, but the vertical bar delimiter is still required.
-                        $footer = __('ORGANIZATION NAME AND URL ARE MISSING FROM THEME CONFIGURATION');
-                    }
-                    else
-                    {
-                        $organizationName = $parts[0];
-                        $organizationUrl = $parts[1];
-                        $organizationLink = empty($parts[1]) ? $organizationName : "<a href='$organizationUrl' title='$organizationName'>$organizationName</a>";
-                        $avantLogicLink = "Created by <a href='http://avantlogic.com/' target='_blank' title='AvantLogic Corporation' rel='noreferrer noopener'>AvantLogic</a>";
-                        $loginUrl = url('users/login');
-                        $logoutUrl = url('users/logout');
-
-                        // Emit both login and logout links, but only one will appear depending whether the 'logged-out'
-                        // or the 'logged-in' class has been dynamically emitted by AvantAdmin::emitDynamicCss().
-                        $loginLogoutLink = "<a class='logged-out' href='$loginUrl'>Login</a><a class='logged-in' href='$logoutUrl'>Logout</a>";
-
-                        $copyright = "© " . date("Y");
-                        $footer = "$copyright $organizationLink — $avantLogicLink — $loginLogoutLink";
-                    }
-                    echo $footer;
+                    $footer = "$themeOptionFooterText<br/>";
                 }
                 else
                 {
-                    echo $themeOptionFooterText;
+                    $footer = "$copyright $siteTitle";
                 }
+                $footer .= "<div id='footer-loginout'>$avantLogicLink — $loginLogoutLink</div>";
+                echo $footer;
+
                 fire_plugin_hook('public_footer', array('view'=>$this));
                 ?>
 			</div>
